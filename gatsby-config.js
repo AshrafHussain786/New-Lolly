7be0580c4b0a2c3d39555b9059c93cb20146830f
MuhammadAshraf <ashrafhussain786@yahoo.com>
2021-04-07 23:39:12 +0500

    updated gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,18 @@ module.exports = {
     author: `@Ashraf`,
   },
   flags: { PRESERVE_WEBPACK_CACHE: true },
-  plugins: [],
+  plugins: [
+    // {
+    //   resolve: "gatsby-source-graphql",
+    //   options: {
+    //     // Arbitrary name for the remote schema Query type
+    //     typeName: "lolly",
+    //     // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+    //     fieldName: "vCards",
+    //     // Url to query from
+    //      url: `https://new-lolly-ashraf.netlify.app/.netlify/functions/vCard`,
+    //     // url: `http://localhost:8888/.netlify/functions/vLolly`,
+    //   },
+    // }
+  ],
 }
